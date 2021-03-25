cfe51ff0b8b4aaac64235e56bd6dc01a875e4227
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-25 07:06:11 +0530

    Add files via upload

---
+++ Tricky4.c
@@ -0,0 +1,12 @@
+//2.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+unsigned int i=65535;
+while(i++!=0)
+{printf("%d",++i);}
+printf("\n");
+return 0;
+}
+//O/P
+// 1..............................................................
