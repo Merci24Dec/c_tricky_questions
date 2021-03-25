e40aad48f95551779acc46a56f822d740e1bceb0
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-25 07:05:56 +0530

    Add files via upload

---
+++ Tricky3.c
@@ -0,0 +1,14 @@
+//2.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+int a=500,b=100,c;
+if(!a>=400)
+{b=300;}
+c=200;
+printf("b=%d c=%d\n",b,c);
+return 0;
+}
+
+//O/P
+// b=100 c=200
