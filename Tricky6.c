9d4c040e33c6918f280cd6c7caddcd4657e00133
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-26 12:18:11 +0530

    Add files via upload

---
+++ Tricky6.c
@@ -0,0 +1,14 @@
+//6.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+char ch;
+if(ch=printf(""))
+   printf("It matters\n");
+else
+   printf("It doesn't matter\n");
+return 0;
+}
+
+//O/P
+//It doesn't matter 
