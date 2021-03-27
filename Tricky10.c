afe86611314e211ee35bcf454743acdff8957311
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-27 22:49:34 +0530

    Add files via upload

---
+++ Tricky10.c
@@ -0,0 +1,11 @@
+//10.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+int x[]= {10,20,30,40,50};
+printf("\n %d %d %d %d %d ",x[4],3[x],x[2],1[x],x[0]);
+return 0;
+}
+
+//O/P
+// 50 40 30 20 10
