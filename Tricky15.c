364f2790f67f9fa63d26fe88e4c9b47331333eeb
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-27 22:55:07 +0530

    Add files via upload

---
+++ Tricky15.c
@@ -0,0 +1,12 @@
+//15.Find the output of the following program
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[]="Hello World";
+printf("\n%ld,%ld",sizeof(a),strlen(a));
+return 0;
+}
+
+//O/P
+// 12,11
