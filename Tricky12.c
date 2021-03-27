c986f9f284af7d2454827c1d92d8ce5dc5db3144
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-27 22:51:16 +0530

    Add files via upload

---
+++ Tricky12.c
@@ -0,0 +1,11 @@
+//12.Find the output of the following program
+#include<stdio.h>
+#define square(x) x*x
+int main()
+{
+printf("\n square = %d", square(10+2));
+return 0;
+}
+
+//O/P
+// square = 32
