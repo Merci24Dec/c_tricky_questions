c1335e8714eed1d5bde6cf8e364741e29079733b
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-27 22:52:52 +0530

    Add files via upload

---
+++ Tricky13.c
@@ -0,0 +1,13 @@
+//13.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+int x;
+x= printf("I See, Sea in C");
+printf("\n x =%d",x);
+return 0;
+}
+
+//O/P
+// I See, Sea in C
+// x = 15
