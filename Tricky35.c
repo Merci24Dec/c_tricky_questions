12768aa624735775f123dd3682b16d54390d0d2f
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-30 06:16:00 +0530

    Add files via upload

---
+++ Tricky35.c
@@ -0,0 +1,13 @@
+//35.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+float x=10.7;
+int i;
+i=(int)x; //explicit type casting
+printf("%d",i);
+return 0;
+}
+
+//O/P
+// 10
