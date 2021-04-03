1fcf8810ffbfca66707d0a391b11406836d5bc4b
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-03 06:04:50 +0530

    Add files via upload

---
+++ Tricky44.c
@@ -0,0 +1,17 @@
+//44.Find the output of the following program
+#include<stdio.h>
+int main()
+{
+int a=2;
+switch(a)
+{
+case 1:printf("goodbye");
+break;
+case 2:continue;
+case 3:printf("bye");
+}
+return 0;
+}
+
+//O/P
+// Error
