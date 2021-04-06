437f49cf98c75e8badf4bf29728c2ef93ba11e32
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-06 05:48:30 +0530

    Add files via upload

---
+++ Tricky69.c
@@ -0,0 +1,10 @@
+//69.Find the output of the following program
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+printf("%ld, %ld",sizeof("akash"), strlen("akash"));
+return 0;
+}
+//O/P
+//6, 5
