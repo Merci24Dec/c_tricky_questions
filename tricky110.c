8331bc1d294a78573a2f9b4747dd0c0de885a0ce
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-10 14:44:58 +0530

    Create tricky110.c

---
+++ tricky110.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a=2,b=3,cout=0,cout1=0;
+    while(a<=5)
+    {
+        b=2;
+        while(b<=4)
+        {
+            b=b+2;
+            cout1++;
+        }
+        a++;
+        cout++;
+    }
+    printf("%d %d",cout,cout1);
+}
